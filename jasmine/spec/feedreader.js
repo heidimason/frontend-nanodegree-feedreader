1b3f17d3100a52ba3524b2497b618a0e5f141a48
Heidi Mason <hapaheidi@gmail.com>
2015-07-07 15:27:08 -0700

    Add 6th test, refactor

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,36 +31,51 @@ $(function() {
     });
 
     describe('The menu', function() {
-        var body = $('body');
+        var $body = $('body');
 
         it('is hidden', function() {
-            expect(body).toHaveClass('menu-hidden');
+            expect($body).toHaveClass('menu-hidden');
         });
 
         it('toggles', function() {
-            var menuIcon = $('.menu-icon-link');
+            var $menuIcon = $('.menu-icon-link');
 
-            menuIcon.click();
-            expect( body ).not.toHaveClass('menu-hidden');
+            $menuIcon.click();
+            expect($body).not.toHaveClass('menu-hidden');
 
-            menuIcon.click();
-            expect( body ).toHaveClass('menu-hidden');
+            $menuIcon.click();
+            expect($body).toHaveClass('menu-hidden');
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        var $entry = $('article.entry');
+        var $feedContainer = $('.feed');
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        beforeEach(function(done) { // Using "beforeEach()" because loadFeed() is asynchronous
+            loadFeed(0, done);
+         });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        it('are in feed container', function(done) {
+            expect( ($entry, $feedContainer).length ).not.toBe(0);
+            done();
+        });
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    // describe('New Feed Selection', function() {
+    //     /* TODO: Write a test that ensures when a new feed is loaded
+    //      * by the loadFeed function that the content actually changes.
+    //      * Remember, loadFeed() is asynchronous.
+    //      */
+    //     beforeEach(function(done) { // Using "beforeEach()" because loadFeed() is asynchronous
+    //         loadFeed(function() {
+    //             done();
+    //         });
+    //      });
+
+    //     it('content changes', function(done) {
+    //         // expect($container).;
+    //         done();
+    //     });
+    // });
 }());
